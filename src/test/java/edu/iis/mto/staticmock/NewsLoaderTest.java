bc5f79c8e2a89741171874fb3df2b85fec1d3ccc
Bogost <bogumilostapiuk@gmail.com>
2019-05-15 22:19:21 +0200

    dodanie testu CheckIfInfoForSubscriptedIsAdded1TimeIfIncomingNewsHasOneInfoForSubscripted()

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -64,12 +64,41 @@ public class NewsLoaderTest {
         /*
          * verify(publishableNews, times(1)).addPublicInfo("NONE1"); verify(publishableNews,
          * times(1)).addPublicInfo("NONE2"); verify(publishableNews, times(2)).addPublicInfo(any(String.class));
-         * 
+         *
          * verify(publishableNews, times(1)).addForSubscription("A", SubsciptionType.A); verify(publishableNews,
          * times(1)).addForSubscription("B", SubsciptionType.B); verify(publishableNews,
          * times(1)).addForSubscription("C", SubsciptionType.C); verify(publishableNews,
          * times(3)).addForSubscription(any(String.class), any(SubsciptionType.class));
          */
+    }
+
+    @Test
+    public void CheckIfInfoForSubscriptedIsAdded1TimeIfIncomingNewsHasOneInfoForSubscripted() {
+        IncomingNews incomingNews;
+        NewsLoader newsLoader;
+
+        incomingNews = new IncomingNews();
+        incomingNews.add(new IncomingInfo("C", SubsciptionType.C));
+        incomingNews.add(new IncomingInfo("NONE1", SubsciptionType.NONE));
+        incomingNews.add(new IncomingInfo("NONE1", SubsciptionType.NONE));
+
+        NewsReader newsReader = Mockito.mock(NewsReader.class);
+        Mockito.when(newsReader.read())
+               .thenReturn(incomingNews);
+
+        mockStatic(NewsReaderFactory.class);
+
+        when(NewsReaderFactory.getReader(any(String.class))).thenReturn(newsReader);
+
+        newsLoader = new NewsLoader();
+
+        PublishableNews publishableNews = Mockito.mock(PublishableNews.class);
+
+        mockStatic(PublishableNews.class);
+        when(PublishableNews.create()).thenReturn(publishableNews);
+
+        newsLoader.loadNews();
+        verify(publishableNews, times(1)).addForSubscription(any(String.class), any(SubsciptionType.class));
     }
 
 }
